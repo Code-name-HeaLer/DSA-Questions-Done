7419c9591bbc01ecc95e17d615386bd370b81e91
CodeNameHealer <swagatprasad3344@gmail.com>
2025-08-01 08:15:38 +0530

    A2Z DSA, prob - 2(If else if ladder)

---
+++ TUF-A2Z-DSA/Things-to-know-basics/If-ElseIf.js
@@ -0,0 +1,44 @@
+/**
+ If ElseIf
+
+ Given marks of a student, print on the screen:
+
+Grade A if marks >= 90
+Grade B if marks >= 70
+Grade C if marks >= 50
+Grade D if marks >= 35
+Fail, otherwise.
+
+
+For printing use:-
+for Javascript : console.log()
+
+Examples:
+Input: marks = 95
+
+Output: Grade A
+
+Explanation: marks are greater than or equal to 90.
+
+Input: marks = 14
+
+Output: Fail
+
+Explanation: marks are less than 35.
+ */
+
+class Solution {
+  studentGrade(marks) {
+    if (marks >= 90) {
+      console.log("Grade A");
+    } else if (marks >= 70) {
+      console.log("Grade B");
+    } else if (marks >= 50) {
+      console.log("Grade C");
+    } else if (marks >= 350) {
+      console.log("Grade D");
+    } else {
+      console.log("Fail");
+    }
+  }
+}
