db9f6ba21ada98f35f28bd47f0a3459ef4adeaae
CodeNameHealer <swagatprasad3344@gmail.com>
2025-08-01 08:10:52 +0530

    A2Z DSA, prob - 1(I/O)

---
+++ TUF-A2Z-DSA/Things-to-know-basics/Input-Output.js
@@ -0,0 +1,23 @@
+/**
+ Input Output
+
+ Complete the function printNumber which takes an integer input from the user and prints it on the screen.
+
+Use:-
+for Javascript : console.log()
+
+Examples:
+Input(user gives value): 7
+
+Output: 7
+
+Input(user gives value): -5
+
+Output: -5
+ */
+
+class Solution {
+  printNumber(value) {
+    console.log(value);
+  }
+}
